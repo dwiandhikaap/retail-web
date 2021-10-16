347186465af8402c72fac9a934f09489ce5d0bfb
Dwi Andhika <dwiandhikaaji@gmail.com>
2021-10-16 12:01:39 +0700

    product query api is now working
    only supports sort by price, page, and category querying for now
    will add product name querying at some point

--- Util/DatabaseHandler/Barang.js
+++ Util/DatabaseHandler/Barang.js
@@ -1,4 +1,5 @@
 const { sqlQuery } = require("../DatabaseHandler");
+const { clamp } = require("../Utility");
 
 async function dbIsStockEnough(productId, quantity){
     const queryString = `
@@ -7,7 +8,6 @@ async function dbIsStockEnough(productId, quantity){
 
     return (await sqlQuery(queryString))[0][0].stock >= quantity;
 }
-
 
 async function dbDecreaseBarangStock(barangId, decrement){
     const queryString = `
@@ -29,8 +29,74 @@ async function dbIncreaseBarangSold(barangId, increment){
     await sqlQuery(queryString);
 }
 
+async function dbGetBarangAvailablePage(category){
+    let queryString = `
+        SELECT * from barang
+        WHERE stock > 0
+    `
+    if(category){
+        queryString += ` AND category="${category}"`;
+    }
+
+    const itemsFound = (await sqlQuery(queryString+';'))[0];
+
+    return Math.ceil(Object.keys(itemsFound).length/10);
+}
+
+async function dbGetBarangList(category, sortMode, page){
+    const availablePage = await dbGetBarangAvailablePage(category);
+
+    if(page > availablePage){
+        return {
+            availablePage: availablePage,
+            items: {}
+        }
+    }
+
+    let queryString = `
+        SELECT * from barang
+        WHERE stock > 0
+    `
+    if(category){
+        queryString += ` AND category="${category}"`;
+    }
+
+    let orderString = '';
+    switch(sortMode.toUpperCase()){
+        case "PRICE_LOW": {
+            orderString = "ASC";
+            break;
+        }
+        case "PRICE_HIGH": {
+            orderString = "desc";
+            break;
+        }
+    }
+
+    if(orderString){
+        queryString += ` ORDER BY price ${orderString}`;
+    }
+
+    queryString += ` LIMIT 10`;
+    
+    if(page){
+        const offset = clamp(page-1, 0, 99)*10;
+        queryString += ` OFFSET ${offset}`;
+    }
+
+
+
+    const itemFound = (await sqlQuery(queryString+';'))[0];
+
+    return {
+        availablePage: availablePage,
+        items: itemFound
+    }
+}
+
 module.exports = {
     dbDecreaseBarangStock : dbDecreaseBarangStock,
     dbIsStockEnough : dbIsStockEnough,
-    dbIncreaseBarangSold: dbIncreaseBarangSold
+    dbIncreaseBarangSold: dbIncreaseBarangSold,
+    dbGetBarangList: dbGetBarangList
 }
--- routes/api/v1/product.js
+++ routes/api/v1/product.js
@@ -1,12 +1,32 @@
 const { Router } = require("express");
-const { dbGetData } = require("../../../Util/DatabaseHandler")
+const { dbGetData, dbGetBarangList } = require("../../../Util/DatabaseHandler/Barang");
+const { isRequestQueryValid } = require("../../../Util/Utility");
 
 const router = Router()
 
 router.get("/shop_items", async(req, res) => {
-    res.send(
-        await dbGetData("barang", 1,10)
-    )
+    let category = req.query.category;
+    let sortMode = req.query.sort || "";
+    let page = parseInt(req.query.page);
+
+    for(const query of [category, sortMode]){
+        if(query){
+            if(isRequestQueryValid(query)){
+                res.status(400).send("Invalid request query!");
+                return;
+            }
+        }
+    };
+
+    try {
+        var barangData = await dbGetBarangList(category, sortMode, page);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("Internal server error!");
+        return;
+    }
+
+    res.status(200).send(barangData);
 })
 
 router.get("/product_data", async(req, res) => {
