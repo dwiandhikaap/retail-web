9972a7a09a5438771d7eeacfc18ee1a5c589920f
Dwi Andhika <dwiandhikaaji@gmail.com>
2021-10-16 12:00:05 +0700

    added request query validation function
    it allows alphanumeric and '_' char

--- Util/Utility.js
+++ Util/Utility.js
@@ -9,6 +9,16 @@ function isStringSus(string){
     return !(string.match("^[A-Za-z0-9]+$"));
 }
 
+function isRequestQueryValid(string){
+    return !(string.match("^[A-Za-z0-9_]+$"));
+}
+
+function clamp(num, min, max){
+    return (num > max ? max : (num < min ? min : num))
+}
+
 module.exports = {
-    isStringSus: isStringSus
+    isStringSus: isStringSus,
+    isRequestQueryValid: isRequestQueryValid,
+    clamp: clamp
 }
