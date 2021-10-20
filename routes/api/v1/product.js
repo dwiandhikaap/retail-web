03c5a4a6848a3760834993ea4aec9c26a5c9b97c
Dwi Andhika <dwiandhikaaji@gmail.com>
2021-10-20 21:42:35 +0700

    /api/v1/product - added product image api

--- routes/api/v1/product.js
+++ routes/api/v1/product.js
@@ -1,6 +1,8 @@
 const { Router } = require("express");
 const { dbGetBarangList, dbGetBarang, dbGetCategoryList } = require("../../../Util/DatabaseHandler/Barang");
 const { isRequestQueryValid } = require("../../../Util/Utility");
+const fs = require("fs")
+const path = require("path")
 
 const router = Router()
 
@@ -31,12 +33,12 @@ router.get("/shop_items", async(req, res) => {
 
 router.get("/product_data", async(req, res) => {
     const productId = parseInt(req.query.id)
-
+    
     if(isNaN(productId) || productId < 0){
         res.status(400).end();
         return;
     }
-
+    
     res.send(
         await dbGetBarang(productId)
     )
@@ -46,6 +48,29 @@ router.get("/get_category_list", async(req, res) => {
     const categoryList = (await dbGetCategoryList()).map(val => Object.values(val)[0]);
 
     res.send(categoryList);
+})
+
+router.get("/img", async(req,res) => {
+    const id = parseInt(req.query.id);
+    if(isNaN(id)){
+        res.status(400).end();
+        return;
+    }
+
+    const imgPath = path.join(__dirname, '../../../public/product/img');
+    const fileExtensions = [".jpg", ".jpeg", ".png"]
+
+    for(extension of fileExtensions){
+        if(fs.existsSync(path.join(imgPath, `${id}${extension}`))){
+            res.sendFile(`${id}${extension}`, {
+                root: imgPath
+            })
+            return;
+        }
+    }
+
+    res.status(404).end()
+
 })
 
 module.exports = router;
